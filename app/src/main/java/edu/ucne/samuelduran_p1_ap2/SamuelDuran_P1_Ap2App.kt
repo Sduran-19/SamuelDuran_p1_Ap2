43dd16340cb494b91de5bc92586deae6ad3cd6d6
SDuran-19 <samuelduran1919@gmail.com>
2024-09-18 20:04:32 -0400

    Agregando hilt

---
+++ app/src/main/java/edu/ucne/samuelduran_p1_ap2/SamuelDuran_P1_Ap2App.kt
@@ -0,0 +1,9 @@
+package edu.ucne.samuelduran_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SamuelDuran_P1_Ap2App : Application() {
+
+}
