70f9a663f36fa5567948c4d0faea6d0df7e1d269
SDuran-19 <samuelduran1919@gmail.com>
2024-09-25 20:43:14 -0400

    agregando screen

--- app/src/main/java/edu/ucne/samuelduran_p1_ap2/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/samuelduran_p1_ap2/presentation/navigation/Screen.kt
@@ -3,12 +3,23 @@ package edu.ucne.samuelduran_p1_ap2.presentation.navigation
 import kotlinx.serialization.Serializable
 
 sealed class Screen {
+    @Serializable
+    object ListScreen : Screen() {
+        const val route = "list_screen"
+    }
 
     @Serializable
-    data object ListScreen : Screen()
+    object RegistroScreen : Screen() {
+        const val route = "registro_screen"
+    }
 
     @Serializable
-    data class RegistroScreen(val id: Int) : Screen()
+    data class EditAlgoScreen(val algoId: Int) : Screen() {
+        val route = "edit_algo_screen/$algoId"
+    }
 
-
+    @Serializable
+    data class DeleteAlgoScreen(val algoId: Int) : Screen() {
+        val route = "delete_algo_screen/$algoId"
+    }
 }
